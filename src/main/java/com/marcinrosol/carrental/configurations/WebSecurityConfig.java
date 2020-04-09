729e7b5aa9aee26eb3b282df3052c57a624e135a
Marcin <marcinrosol1997@gmail.com>
2020-04-09 14:39:57 +0200

    commit

--- src/main/java/com/marcinrosol/carrental/configurations/WebSecurityConfig.java
+++ src/main/java/com/marcinrosol/carrental/configurations/WebSecurityConfig.java
@@ -1,27 +1,11 @@
 package com.marcinrosol.carrental.configurations;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/api/cars").authenticated()
-                .antMatchers("/api/rent").authenticated()
-                .antMatchers("/api/user").authenticated();
-
-    }
 
 
 
--- src/main/java/com/marcinrosol/carrental/models/User.java
+++ src/main/java/com/marcinrosol/carrental/models/User.java
@@ -1,13 +1,12 @@
 package com.marcinrosol.carrental.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class User {
@@ -30,6 +29,11 @@ public class User {
     @NotBlank
     @Email
     private String name; //name as an email, it'll be needed to spring security and to fetching user information from db
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
 
     public Long getId() {
         return id;
@@ -63,6 +67,14 @@ public class User {
         this.name = name;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -70,6 +82,7 @@ public class User {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", name='" + name + '\'' +
-                '}';
+                                '}';
     }
+
 }
